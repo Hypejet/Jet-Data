8743303409cd148e7465d69a47265e6cf07bdf0e
codestech <ryszardcichoszotawski@gmail.com>
2024-07-05 07:16:02 +0200

    misc: remove entity spawn settings

--- model/src/main/java/net/hypejet/jet/data/biome/Biome.java
+++ model/src/main/java/net/hypejet/jet/data/biome/Biome.java
@@ -3,10 +3,8 @@ package net.hypejet.jet.data.biome;
 import net.hypejet.jet.data.IdentifiedRegistryEntry;
 import net.hypejet.jet.data.biome.effects.BiomeEffects;
 import net.hypejet.jet.data.biome.settings.ClimateSettings;
-import net.hypejet.jet.data.biome.settings.EntitySpawnSettings;
 import net.kyori.adventure.key.Key;
 import org.checkerframework.checker.nullness.qual.NonNull;
-import org.checkerframework.checker.nullness.qual.Nullable;
 
 import java.util.Objects;
 
@@ -37,15 +35,6 @@ public sealed interface Biome extends IdentifiedRegistryEntry permits BiomeImpl 
     @NonNull BiomeEffects effects();
 
     /**
-     * Gets an {@linkplain EntitySpawnSettings entity spawn settings} of the biome.
-     *
-     * @return the entity spawn settings
-     * @since 1.0
-     * @see EntitySpawnSettings
-     */
-    @Nullable EntitySpawnSettings entitySpawnSettings();
-
-    /**
      * Creates a {@linkplain Biome biome}.
      *
      * @param key a key of the biome
@@ -55,24 +44,9 @@ public sealed interface Biome extends IdentifiedRegistryEntry permits BiomeImpl 
      * @since 1.0
      */
     static @NonNull Biome biome(@NonNull Key key, @NonNull ClimateSettings climate, @NonNull BiomeEffects effects) {
-        return biome(key, climate, effects, null);
-    }
-
-    /**
-     * Creates a {@linkplain Biome biome}.
-     *
-     * @param key a key of the biome
-     * @param climate a climate settings of the biome
-     * @param effects effects of the biome
-     * @param entitySpawnSettings an entity spawn settings of the biome
-     * @return the biome
-     * @since 1.0
-     */
-    static @NonNull Biome biome(@NonNull Key key, @NonNull ClimateSettings climate, @NonNull BiomeEffects effects,
-                                @Nullable EntitySpawnSettings entitySpawnSettings) {
         Objects.requireNonNull(key, "The key must not be null");
         Objects.requireNonNull(climate, "The climate settings must not be null");
         Objects.requireNonNull(effects, "The biome settings must not be null");
-        return new BiomeImpl(key, climate, effects, entitySpawnSettings);
+        return new BiomeImpl(key, climate, effects);
     }
 }
--- model/src/main/java/net/hypejet/jet/data/biome/BiomeImpl.java
+++ model/src/main/java/net/hypejet/jet/data/biome/BiomeImpl.java
@@ -2,7 +2,6 @@ package net.hypejet.jet.data.biome;
 
 import net.hypejet.jet.data.biome.effects.BiomeEffects;
 import net.hypejet.jet.data.biome.settings.ClimateSettings;
-import net.hypejet.jet.data.biome.settings.EntitySpawnSettings;
 import net.kyori.adventure.key.Key;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
@@ -13,10 +12,8 @@ import org.checkerframework.checker.nullness.qual.Nullable;
  * @param key a key of the biome
  * @param climate climate settings of the biome
  * @param effects effects of the biome
- * @param entitySpawnSettings entity spawn settings of the biome
  * @since 1.0
  * @author Codestech
  * @see Biome
  */
-record BiomeImpl(@NonNull Key key, @NonNull ClimateSettings climate, @NonNull BiomeEffects effects,
-                 @Nullable EntitySpawnSettings entitySpawnSettings) implements Biome {}
+record BiomeImpl(@NonNull Key key, @NonNull ClimateSettings climate, @NonNull BiomeEffects effects) implements Biome {}
