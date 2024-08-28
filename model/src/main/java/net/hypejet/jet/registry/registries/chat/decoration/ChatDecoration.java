33135d956dba4c9b34669c6b1b7349e09ccc00d8
codestech <ryszardcichoszotawski@gmail.com>
2024-08-28 17:55:55 +0200

    feat: chat type models

---
+++ model/src/main/java/net/hypejet/jet/registry/registries/chat/decoration/ChatDecoration.java
@@ -0,0 +1,38 @@
+package net.hypejet.jet.registry.registries.chat.decoration;
+
+import net.hypejet.jet.registry.registries.chat.ChatType;
+import net.hypejet.jet.utils.NullabilityUtil;
+import net.kyori.adventure.key.Key;
+import net.kyori.adventure.text.format.Style;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+import java.util.List;
+
+
+/**
+ * Represents a decoration of a {@linkplain ChatType chat type}.
+ *
+ * @param translationKey an identifier of a translation of the decoration
+ * @param style a style that should be applied to messages with chat type using the decoration
+ * @param parameters placeholders which should be used during translation using the translation key specified
+ * @since 1.0
+ * @author Codestech
+ * @see ChatType
+ */
+public record ChatDecoration(@NonNull Key translationKey, @Nullable Style style,
+                             @NonNull List<ChatDecorationParameter> parameters) {
+    /**
+     * Constructs the {@linkplain ChatDecoration chat decoration}.
+     *
+     * @param translationKey an identifier of a translation of the decoration
+     * @param style a style that should be applied to messages with chat type using the decoration
+     * @param parameters placeholders which should be used during translation using the translation key specified
+     * @since 1.0
+     */
+    public ChatDecoration {
+        NullabilityUtil.requireNonNull(translationKey, "translation key");
+        NullabilityUtil.requireNonNull(parameters, "parameters");
+        parameters = List.copyOf(parameters);
+    }
+}
