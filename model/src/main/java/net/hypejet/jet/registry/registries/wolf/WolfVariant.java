edf3e2dad71954173fc2baeaecef15283b818d93
codestech <ryszardcichoszotawski@gmail.com>
2024-09-01 19:01:55 +0200

    feat: wolf variant biomes

---
+++ model/src/main/java/net/hypejet/jet/registry/registries/wolf/WolfVariant.java
@@ -0,0 +1,17 @@
+package net.hypejet.jet.registry.registries.wolf;
+
+import net.kyori.adventure.key.Key;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+/**
+ * Represents a variant of a Minecraft wolf.
+ *
+ * @param wildTexture a texture of a wild version of wolves with this variant
+ * @param tameTexture a texture of a tame version of wolves with this variant
+ * @param angryTexture a texture of an angry version of wolves with this variant
+ * @param biomes biomes on which wolves with this type can spawn
+ * @since 1.0
+ * @author Codestech
+ */
+public record WolfVariant(@NonNull Key wildTexture, @NonNull Key tameTexture, @NonNull Key angryTexture,
+                          @NonNull WolfBiomes biomes) {}
